f9137efdce42425d336af96b1dd000cd6008942f
Ivan_Kozlenko <Ivan_Kozlenko@epam.com>
2021-01-11 17:10:18 +0300

    Illegal chars done

--- homework/illegal_char/illegal_char_remover.py
+++ homework/illegal_char/illegal_char_remover.py
@@ -1,4 +1,7 @@
+import re
+
 from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.functions import regexp_replace
 from typing import List
 
 
@@ -7,7 +10,15 @@ class IllegalCharRemover:
     Class provides possibilities to remove illegal chars from string column.
     """
     def __init__(self, chars: List[str], replacement):
-        pass
+        if not chars or replacement is None:
+            raise ValueError
+        self.pattern = '|'.join(map(re.escape, chars))
+        self.replacement = replacement
 
     def remove_illegal_chars(self, dataframe: DataFrame, source_column: str, target_column: str):
-        pass
+        df = dataframe.withColumn(
+            target_column,
+            regexp_replace(dataframe[source_column], self.pattern, self.replacement),
+        )
+        df = df.drop(source_column)
+        return df
