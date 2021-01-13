ef857aadb928d759c30c3e783a094f437affcac9
Ivan_Kozlenko <Ivan_Kozlenko@epam.com>
2021-01-13 15:33:25 +0300

    Schema merging 3/4 tests work

--- homework/schema_merge_union/schema_merging.py
+++ homework/schema_merge_union/schema_merging.py
@@ -1,4 +1,5 @@
 from pyspark.sql import DataFrame
+from pyspark.sql import functions
 
 
 class SchemaMerging:
@@ -10,6 +11,54 @@ class SchemaMerging:
     {field_name}_{field_type}
     """
 
-    # ToDo: Implement dataset union with schema merging
     def union(self, dataframe1: DataFrame, dataframe2: DataFrame):
-        pass
+        col_order = dataframe1.dtypes + [x for x in dataframe2.dtypes if x not in dataframe1.dtypes]
+        col_mapping = self._get_col_mapping(dataframe1, dataframe2)
+
+        # add missing columns for each dataframe
+        dataframe1 = self._process_dataframe(dataframe1, 0, col_mapping, col_order)
+        dataframe2 = self._process_dataframe(dataframe2, 1, col_mapping, col_order)
+
+        return dataframe1.unionByName(dataframe2)
+
+    @staticmethod
+    def _get_col_types(df: DataFrame):
+        return [x[1] for x in df.dtypes]
+
+    @staticmethod
+    def _has_same_name_with_different_type(col_name: str, col_mapping: dict) -> bool:
+        '''If column with given has different data types in given dataframes'''
+        return ((col_name in col_mapping[0]) and (col_name in col_mapping[1])) and \
+               (col_mapping[0][col_name] != col_mapping[1][col_name])
+
+    @staticmethod
+    def _get_col_mapping(df1: DataFrame, df2: DataFrame) -> dict:
+        col_mapping = {
+            0: dict(df1.dtypes),
+            1: dict(df2.dtypes)
+        }
+        return col_mapping
+
+    @staticmethod
+    def _rename_col_with_type(col_name, df_index, col_mapping):
+        return f'{col_name}_{col_mapping[df_index][col_name]}'
+
+    def _process_dataframe(self, df: DataFrame, index: int, col_mapping: dict, col_order: list):
+        for col_name, col_type in col_order:
+            if (col_name, col_type) in df.dtypes:
+                if self._has_same_name_with_different_type(col_name, col_mapping):
+                    df = df.withColumnRenamed(
+                        col_name,
+                        self._rename_col_with_type(col_name, index, col_mapping)
+                    )
+            else:
+                if self._has_same_name_with_different_type(col_name, col_mapping):
+                    # using XOR on index to get the other index
+                    new_col_name = self._rename_col_with_type(col_name, index ^ 1, col_mapping)
+                else:
+                    new_col_name = col_name
+                df = df.withColumn(
+                    new_col_name,
+                    functions.lit(None).cast(col_mapping[index ^ 1][col_name])
+                )
+        return df
