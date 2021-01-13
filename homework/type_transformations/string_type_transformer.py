c860cb629f9a85cf9ae10d871a8594c8f0372aaf
Ivan_Kozlenko <Ivan_Kozlenko@epam.com>
2021-01-13 14:14:21 +0300

    String type transformer done but doesn't fully work

--- homework/type_transformations/string_type_transformer.py
+++ homework/type_transformations/string_type_transformer.py
@@ -1,10 +1,14 @@
 from pyspark.sql import DataFrame
 from pyspark.sql.types import StructType
 
+
 class StringTypeTransformer:
     """
     Class provides transformation for columns with string types to other, where it possible.
     """
 
     def transform_dataframe(self, dataframe: DataFrame, expected_schema: StructType):
-        pass
+        for col_name, expected_type in zip(dataframe.columns, expected_schema):
+            dataframe = dataframe.withColumn(col_name, dataframe[col_name].cast(expected_type.dataType))
+            dataframe.schema[col_name].nullable = expected_type.nullable  # TODO: doesn't work
+        return dataframe
