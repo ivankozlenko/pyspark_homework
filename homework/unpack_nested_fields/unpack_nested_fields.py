ddb9597d2845a8cd4a2dcca671c1536cae708a5b
Ivan_Kozlenko <Ivan_Kozlenko@epam.com>
2021-01-13 16:59:39 +0300

    Unpack nested fields done

--- homework/unpack_nested_fields/unpack_nested_fields.py
+++ homework/unpack_nested_fields/unpack_nested_fields.py
@@ -1,4 +1,5 @@
 from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.functions import explode
 
 
 class UnpackNestedFields:
@@ -9,6 +10,21 @@ class UnpackNestedFields:
 
     """
 
-    # ToDo implement unpacking of nested fields
     def unpack_nested(self, dataframe: DataFrame):
-        pass
+        for col_name, col_type in dataframe.dtypes:
+            if col_type.startswith('array<'):
+                dataframe = self._unpack_array(dataframe, col_name)
+            elif col_type.startswith('struct<'):
+                dataframe = self._unpack_struct(dataframe, col_name)
+        return dataframe
+
+    def _unpack_array(self, df: DataFrame, array_col_name: str):
+        df = df.withColumn(array_col_name, explode(df[array_col_name]))
+        return self.unpack_nested(df)
+
+    def _unpack_struct(self, df: DataFrame, col_name):
+        sub_df = df.select(col_name + '.*')
+        for subcol_name in sub_df.columns:
+            df = df.withColumn(f'{col_name}_{subcol_name}', df[col_name][subcol_name])
+        df = df.drop(col_name)
+        return self.unpack_nested(df)
