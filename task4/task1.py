1368d2e4fe4afec75ca1bbf8c9aeac6326cfb7e8
Ivan_Kozlenko <Ivan_Kozlenko@epam.com>
2021-01-15 13:27:47 +0300

    task4.task1 done

---
+++ task4/task1.py
@@ -0,0 +1,70 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import to_timestamp
+
+
+spark = SparkSession.builder.master("local").getOrCreate()
+BIDS_PATH = 'input/bids.txt'
+
+
+class ErroneousRecordsProcessor:
+    """
+    Task 1: Erroneous records
+
+        Nothing is perfect neither is the bidding system. From time to time something goes wrong
+    in the bidding file generator in the partner's side, so it includes corrupted records in the stream.
+        A record is corrupted if the value in the third column is not a number but a text
+    with this format: ERROR_(.*)
+
+        The task here is to filter out the corrupted records from the input and count how many occurred
+    from the given type in the given hour.
+        The output should be formatted as comma separated lines containing the date (with hour precision),
+    the error message and the count of such messages in the given hour.
+
+    Example: [05-21-07-2016,ERROR_NO_BIDS_FOR_HOTEL,1]
+    """
+
+    def __init__(self, spark_context, file_path):
+        self.sc = spark_context
+        self.bids_header = [
+            "MotelID", "BidDate", "HU", "UK", "NL", "US", "MX", "AU", "CA",
+            "CN", "KR","BE", "I","JP", "IN", "HN", "GY", "DE"
+        ]
+        self.df = self.sc.read.csv(file_path, header=None)
+        self._process_dataset()
+
+    def count_errors(self, n_rows_to_display: int = 10):
+        errors_df = self._filter_errors()
+        print(f'{errors_df.count()}/{self.df.count()} records corrupted')
+        grouped_errors = self._group_errors_by_hour(errors_df)
+        if n_rows_to_display is None or n_rows_to_display < 1 or not isinstance(n_rows_to_display, int):
+            print(f'Displaying all {len(grouped_errors)} error rows')
+            n_rows_to_display = len(grouped_errors)
+        else:
+            print(f'Displaying first {n_rows_to_display} error rows')
+        print('\n'.join(grouped_errors[:n_rows_to_display]))
+
+    def _filter_errors(self):
+        error_df = self.df.filter(self.df[self.df.columns[2]].rlike('ERROR.*'))
+        return error_df
+
+    def _group_errors_by_hour(self, errors_df):
+        group_by_cols = self.df.columns[1:3]
+        records = errors_df.groupby(*group_by_cols).count().collect()
+        output = []
+        for row in records:
+            output.append(f"{row[group_by_cols[0]]}, {row[group_by_cols[1]]}, {row['count']}")
+        return output
+
+    def _process_dataset(self):
+        timestamp_col_name = self.bids_header[1]
+        for old_col, new_col in zip(self.df.columns, self.bids_header):
+            self.df = self.df.withColumnRenamed(old_col, new_col)
+        self.df = self.df.withColumn(
+            timestamp_col_name,
+            to_timestamp(self.df[timestamp_col_name], format='HH-dd-MM-yyyy')
+        )
+
+
+if __name__ == '__main__':
+    errors = ErroneousRecordsProcessor(spark, BIDS_PATH)
+    errors.count_errors()
